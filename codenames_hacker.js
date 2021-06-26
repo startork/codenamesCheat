73e7b5baab543870ea7eb3e576020dc0bbe80f5a
startork <starsky.t@bluefusesystems.com>
2021-06-26 18:44:56 +0100

    allow input of colour

--- codenames_hacker.js
+++ codenames_hacker.js
@@ -1,10 +1,28 @@
 const fs = require("fs");
-const text = fs.readFileSync("json.txt", { encoding: "utf-8" });
-
-rawList = text.match(/coverCard\/blue\/[0-9]+/g);
-numbers = rawList.map((str) => str.match(/[0-9]+/g));
-words = numbers.map((num) => {
-  const reg = new RegExp(`wordCard/${num}.+?"word":"([A-Z]+)`);
-  return text.match(reg)[1];
+const text = fs.readFileSync("input.txt", { encoding: "utf-8" });
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-console.log(words);
+
+let colour = "blue";
+rl.question("Which colour are you blue or red? (B/r):", function (name) {
+  if (["r", "R", "red", "Red"].includes(name)) {
+    colour = "red";
+  } else if (name === "black") {
+    colour = "black";
+  } else if (!(!name || ["b", "B", "blue", "Blue"].includes(name))) {
+    console.error("This is not a valid colour bro!");
+    process.exit();
+  }
+  const colourRegEx = new RegExp(`coverCard\/${colour}\/[0-9]+`, "g");
+  const rawList = text.match(colourRegEx);
+  const numbers = rawList.map((str) => str.match(/[0-9]+/g));
+  const words = numbers.map((num) => {
+    const reg = new RegExp(`wordCard/${num}.+?"word":"([A-Z]+)`);
+    return text.match(reg)[1];
+  });
+  rl.write(words.join() + "\n");
+  rl.close();
+});
